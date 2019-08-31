839fc3458e4ca5411a331d0332c41499fd467424
Pavel Struhar <pstruhar@gmail.com>
2019-08-31 14:15:52 +0200

    Add error event listener on audio

--- src/Player.js
+++ src/Player.js
@@ -15,6 +15,29 @@ const Player = ({ artist, title, streamUrls, isMobile }) => {
 
     if (state === "playing") {
       audio.current = new Audio(`${streamUrls[0]}?${Date.now()}`);
+      audio.current.addEventListener("error", e => {
+        switch (e.target.error.code) {
+          case e.target.error.MEDIA_ERR_ABORTED:
+            alert("You aborted the video playback.");
+            break;
+          case e.target.error.MEDIA_ERR_NETWORK:
+            alert("A network error caused the audio download to fail.");
+            break;
+          case e.target.error.MEDIA_ERR_DECODE:
+            alert(
+              "The audio playback was aborted due to a corruption problem or because the video used features your browser did not support."
+            );
+            break;
+          case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+            alert(
+              "The video audio not be loaded, either because the server or network failed or because the format is not supported."
+            );
+            break;
+          default:
+            alert("An unknown error occurred.");
+            break;
+        }
+      });
       audio.current.play();
     } else if (state === "stopped") {
       audio.current.pause();
